528c045e088eee13888d605a4b938ca288e9f9d5
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-06-15 17:15:38 +0200

    pagination solved

--- index.js
+++ index.js
@@ -11,20 +11,39 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
-
 async function fetchCharacters(page = 1) {
+  cardContainer.textContent = "";
   const response = await fetch(
     `https://rickandmortyapi.com/api/character?page=${page}`
   );
   const data = await response.json();
+  maxPage = data.info.pages;
+  pagination.textContent = `${page} / ${maxPage} `;
   const characters = data.results;
   characters.forEach((character) => {
     createCharacterCard(character);
   });
-
 }
+
+prevButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (page > 1) {
+    page--;
+    fetchCharacters(page);
+  }
+});
+
+nextButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (page <= maxPage) {
+    page++;
+    fetchCharacters(page);
+  }
+});
+
 fetchCharacters();
