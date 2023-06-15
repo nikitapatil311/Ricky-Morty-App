932ddae4b45397cee060e1c7970cc8b99490d803
Nikita <nikitaspatil311@gmail.com>
2023-06-15 14:48:16 +0200

    add fetch characters

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,19 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+// My Code is here
+
+async function fetchCharacters() {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  const characters = data.results;
+  console.log(characters);
+  // console.log("characters", characters);
+  characters.forEach((character) => {
+    createCharacterCard(character);
+  });
+}
+fetchCharacters();
